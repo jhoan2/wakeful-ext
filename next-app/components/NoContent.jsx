d68850a59ad285432ca9b3e00f1fdb9ef3b7092b
jhoan2 <johnhoang1994@gmail.com>
2023-11-25 12:50:08 -0800

    getting error for Image

--- next-app/components/NoContent.jsx
+++ next-app/components/NoContent.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
+
 export default function NoContent() {
     return (
         <div className='flex-grow flex justify-center items-center'>
             <div className='flex flex-col items-center'> {/* Wrapper div with flex-column */}
                 <div className='rounded-full bg-gray-200 p-10 inline-flex justify-center items-center'>
-                    <Image src={'/next-assets/no-content-cat.png'} className="w-40 h-40 object-cover rounded-full" alt="No Content Cat Image" />
+                    <img src={'/next-assets/no-content-cat.png'} className="w-40 h-40 object-cover rounded-full" alt="No Content Cat Image" />
                 </div>
                 <p className="mt-4 text-2xl font-semibold text-gray-800">No content yet</p>
             </div>
