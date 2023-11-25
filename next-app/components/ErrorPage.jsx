75b3373fba4929f0c0233ae60ece8c8e9e9db9e3
jhoan2 <johnhoang1994@gmail.com>
2023-11-25 11:14:19 -0800

    error page component

---
+++ next-app/components/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ErrorPage({ message }) {
+
+    return (
+        <div className='flex h-screen'>
+            <div className='flex-grow flex justify-center items-center'>
+                <div className='flex flex-col items-center'>
+                    <div className='rounded-full bg-gray-200 p-10 inline-flex justify-center items-center'>
+                        <img src={'/next-assets/error-blue-cat.png'} className="w-40 h-40 object-cover rounded-full" />
+                    </div>
+                    <p className="mt-4 text-2xl font-semibold text-gray-800">{message}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
