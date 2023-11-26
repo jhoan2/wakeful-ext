506f8d9fa2c8d8505561a2edd95c407035681e2f
jhoan2 <johnhoang1994@gmail.com>
2023-11-25 16:36:04 -0800

    render the cards

--- next-app/components/CardList.jsx
+++ next-app/components/CardList.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Card from './Card'
+import NoContent from './NoContent'
 
 export default function CardList({ data, currentTab }) {
-    if (data && !data.node.cards) return null
+    if (!data.edges) return <NoContent />
+    const cardList = data.edges
 
-
-    const { edges } = data.node.cards
-
-    return <div className='m-2'>
-        {edges.map((edge) => { return <Card key={edge.node.id} currentTab={currentTab} content={edge} /> })}
+    return <div className='m-2 space-y-2'>
+        {cardList.map((card) => { return <Card key={card.node.id} currentTab={currentTab} content={card} /> })}
         {/* <Card content={edges[0]} currentTab={currentTab} /> */}
     </div>
 }
--- next-app/components/GetCards.jsx
+++ next-app/components/GetCards.jsx
@@ -1,53 +1,70 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useQuery, gql } from '@apollo/client';
 import CardList from './CardList'
+import { useCeramicContext } from '../context';
+import ErrorPage from './ErrorPage';
+import SkeletonCard from './SkeletonCard';
 
 export default function CardNotes({ currentTab, setCurrentResourceId }) {
   const currentUrl = currentTab.url
-  const GET_CARDS_PER_URL = gql`
-  query GET_CARDS_PER_URL {
-      icarusResourceIndex(
-        first: 1
-        filters: {where: {url: {equalTo: "${currentUrl}"}}}
-      ) {
-        edges {
-          node {
-            title
-            cards(
-              first: 10
-              filters: {where: {deleted: {equalTo: false}}}
-            ) {
-              edges {
-                node {
-                  id
-                  resourceId
-                  quote
-                  annotation
-                  pageYOffset
-                  scrollHeight
-                }
+  const clients = useCeramicContext()
+  const { composeClient } = clients
+  const clientMutationId = composeClient.id
+
+  const GET_CARDS_PER_URL_PER_USER = gql`
+  query MyQuery ($url: String!, $account: ID, $after: String) {
+    icarusResourceIndex(filters: {where: {url: {equalTo: $url}}}, first: 1) {
+      edges {
+        node {
+          cards(account: $account, filters: {where: {deleted: {equalTo: false}}}, first: 10, after: $after) {
+            edges {
+              node {
+                annotation
+                cid
+                id
+                resourceId
+                scrollHeight
+                pageYOffset
+                videoTime
+                quote
               }
             }
           }
         }
       }
-    }`
-
-  const { loading, error, data } = useQuery(GET_CARDS_PER_URL, { skip: !currentUrl });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-  if (data) {
-    if (data.icarusResourceIndex.edges.length < 1) {
-      return <p>New User</p>
-    } else {
-      const resourceId = data.icarusResourceIndex.edges[0]?.node.cards.edges[0]?.node.resourceId
-      setCurrentResourceId(resourceId)
     }
   }
+  `
+
+  const { loading, error, data, fetchMore } = useQuery(GET_CARDS_PER_URL_PER_USER, {
+    variables: { url: currentUrl, account: clientMutationId, after: null },
+    skip: !currentUrl
+  });
+
+  if (loading) return <SkeletonCard />
+  if (error) return <ErrorPage message={error.message} />;
+  const cards = data?.icarusResourceIndex.edges[0]?.node.cards
+
+  const loadMore = () => {
+    // if (cards.pageInfo.hasNextPage) {
+    //   fetchMore({
+    //     variables: {
+    //       after: cards.pageInfo.endCursor,
+    //     },
+    //   })
+    //   console.log(cards)
+    // }
+    console.log(cards)
+
+  }
+
+
+
 
   return <div className='dark:text-white'>
-    {data ? <CardList data={data.icarusResourceIndex.edges[0]} currentTab={currentTab} /> : null}
+    {data ? <CardList data={cards} currentTab={currentTab} /> : null}
+    {/* <div>Something</div>
+    <button onClick={() => loadMore()}>More</button> */}
   </div>
 
 }
