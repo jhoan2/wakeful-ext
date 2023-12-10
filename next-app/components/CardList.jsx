45add2fcc651ce568f7f5a4839f2088b372b5f97
jhoan2 <johnhoang1994@gmail.com>
2023-12-10 10:39:24 -0800

    fix(cardList): error reading undefined

--- next-app/components/CardList.jsx
+++ next-app/components/CardList.jsx
@@ -3,11 +3,11 @@ import Card from './Card'
 import NoContent from './NoContent'
 
 export default function CardList({ data, currentTab }) {
-    if (!data.edges) return <NoContent />
-    const cardList = data.edges
+    if (!data || data?.length === 0) return <NoContent />
+    const cardList = data?.edges
 
     return <div className='m-2 space-y-2'>
-        {cardList.map((card) => { return <Card key={card.node.id} currentTab={currentTab} content={card} /> })}
+        {data.map((card) => { return <Card key={card.node.id} currentTab={currentTab} content={card} /> })}
         {/* <Card content={edges[0]} currentTab={currentTab} /> */}
     </div>
 }
