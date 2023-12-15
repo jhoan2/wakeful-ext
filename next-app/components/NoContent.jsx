4b75f50be2366dd3f49162cc2f046171a8439010
jhoan2 <johnhoang1994@gmail.com>
2023-12-15 09:54:33 -0800

    style(image): set img width and height for optimization

--- next-app/components/NoContent.jsx
+++ next-app/components/NoContent.jsx
@@ -5,7 +5,7 @@ export default function NoContent() {
         <div className='flex-grow flex justify-center items-center h-screen'>
             <div className='flex flex-col items-center'> {/* Wrapper div with flex-column */}
                 <div className='rounded-full bg-gray-200 p-10 inline-flex justify-center items-center'>
-                    <img src={'/next-assets/no-content-cat.png'} className="w-40 h-40 object-cover rounded-full" alt="No Content Cat Image" />
+                    <img src={'/next-assets/no-content-cat.png'} width={160} height={160} className="object-cover rounded-full" alt="No Content Cat Image" />
                 </div>
                 <p className="mt-4 text-2xl font-semibold text-gray-800">No content yet</p>
             </div>
