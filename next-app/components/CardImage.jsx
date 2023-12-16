bdcbb98d26e34350e0415e405b42851a004daeea
jhoan2 <johnhoang1994@gmail.com>
2023-12-16 15:57:34 -0800

    refactor: card image, card list

--- next-app/components/CardImage.jsx
+++ next-app/components/CardImage.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function CardImage({ cid }) {
 
-
-    // if (!cid) return
+    if (!cid) return
 
     return (
         <div>
             <img
-                src={'https://purple-defensive-anglerfish-674.mypinata.cloud/ipfs/QmdCF1CxkKJKN31H9SpeyjV51ZNNmxXfREsR154ZFod78g?img-width=240'}
+                src={`https://purple-defensive-anglerfish-674.mypinata.cloud/ipfs/${cid}?img-width=240`}
                 alt="Image"
                 loading='lazy'
             />
--- next-app/components/CardList.jsx
+++ next-app/components/CardList.jsx
@@ -4,11 +4,9 @@ import NoContent from './NoContent'
 
 export default function CardList({ data, currentTab }) {
     if (!data || data?.length === 0) return <NoContent />
-    const cardList = data?.edges
 
     return <div className='m-2 space-y-2'>
         {data.map((card) => { return <Card key={card.node.id} currentTab={currentTab} content={card} /> })}
-        {/* <Card content={edges[0]} currentTab={currentTab} /> */}
     </div>
 }
 
