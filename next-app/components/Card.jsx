51f42201cbe01a72244f5c794b0582010c551ee3
jhoan2 <johnhoang1994@gmail.com>
2023-12-16 14:54:04 -0800

    fix(cardImage): react suspense didn't work, image not defined, and button should show up if no cid

--- next-app/components/Card.jsx
+++ next-app/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Suspense } from 'react'
+import React, { useState } from 'react'
 import { gql, useMutation } from '@apollo/client';
 import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react';
 import EditorBubbleMenu from './EditorBubbleMenu';
@@ -204,11 +204,9 @@ export default function Card({ content, currentTab }) {
                 </p> :
                 null
             }
-            {!cid ?
+            {cid ?
                 <div className='mb-4'>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <CardImage cid={cid} />
-                    </Suspense>
+                    <CardImage cid={cid} />
                 </div>
 
                 : null
@@ -221,7 +219,7 @@ export default function Card({ content, currentTab }) {
                 <img src={URL.createObjectURL(uploadImage)} alt="Pasted Image" />
             )}
             {inputImage ?
-                <input onPaste={handlePaste} id='paste-image' className={`${image && 'hidden'} placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm`} placeholder="Paste image here" type="text" name="Paste Image" autoComplete='off' />
+                <input onPaste={handlePaste} id='paste-image' className={`${uploadImage && 'hidden'} placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm`} placeholder="Paste image here" type="text" name="Paste Image" autoComplete='off' />
                 :
                 null
             }
@@ -232,7 +230,7 @@ export default function Card({ content, currentTab }) {
                     onChange={(event) => handleUploadImage(event.target.files[0])}
                     className={`${inputImage ? '' : 'invisible'} file:py-2 file:px-3 file:inline-flex file:items-center file:gap-x-2 file:text-sm file:font-semibold file:rounded-lg file:border file:border-transparent file:bg-blue-100 file:text-blue-800 file:hover:bg-blue-200 file:disabled:opacity-50 file:disabled:pointer-events-none file:dark:hover:bg-blue-900 file:dark:text-blue-400 file:dark:focus:outline-none file:dark:focus:ring-1 file:dark:focus:ring-gray-600`} />
                 <div className='flex space-x-3'>
-                    {cid ?
+                    {!cid ?
                         <button onClick={() => setInputImage(!inputImage)} className='py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600' title='Add an image'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='w-6 h-6'><path d="M2.9918 21C2.44405 21 2 20.5551 2 20.0066V3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918ZM20 15V5H4V19L14 9L20 15ZM20 17.8284L14 11.8284L6.82843 19H20V17.8284ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg>
                         </button> :
